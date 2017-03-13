4484a7ebc32f58d5e7931360be6d870692f5a2b6
Sjobus <sibevanetten@hotmail.com>
2017-03-13 12:59:28 +0100

    test classe om te kijken of het bij jullie ook werkt
    ^^

---
+++ src/Test/GitGuudScrub.java
@@ -0,0 +1,12 @@
+package Test;
+
+/**
+ * Created by Sibe on 13-3-2017.
+ * Gebruik deze class om wat dingentjes te oefenenen =3
+ */
+public class GitGuudScrub
+{
+    public static String getMessage(){
+        return "Hoi allemaal hoe gaat het met jullie?";
+    }
+}
