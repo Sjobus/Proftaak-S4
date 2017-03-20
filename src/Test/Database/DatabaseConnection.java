d373481f41a274b9888be8c81e95d963fce5377c
Wartenberg,Koen K.W <k.wartenberg@student.fontys.nl>
2017-03-20 10:28:54 +0100

    database
    ^^^^^^^^^^^^^^^^^^^^

---
+++ src/Test/Database/DatabaseConnection.java
@@ -0,0 +1,51 @@
+package Test.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * Created by Gebruiker on 20-3-2017.
+ */
+public class DatabaseConnection {
+    String user = "root";
+    String pass = "root";
+
+    private Connection connection;
+    private Properties properties;
+
+    //Create properties
+
+    private Properties getProperties() {
+        if (properties == null) {
+            properties = new Properties();
+            properties.setProperty("user", user);
+            properties.setProperty("password", pass);
+        }
+        return properties;
+    }
+
+    //connect database
+    public static Connection connect() {
+        try {
+//                    Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection("jdbc:oracle:thin:@//fhictora01.fhict.local:1521/fhictora", "dbi331842", "Qwerty123");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void disconnect() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
