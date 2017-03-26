ef67e5ca2eff221daac40262ce006b0ac3ff3593
Wartenberg,Koen K.W <k.wartenberg@student.fontys.nl>
2017-03-26 12:53:08 +0200

    c

---
+++ src/Pts4/Classes/staticPerson.java
@@ -0,0 +1,10 @@
+package Pts4.Classes;
+
+/**
+ * Created by Gebruiker on 26-3-2017.
+ */
+public class staticPerson {
+
+    //class is used for keeping track of current logged in the user
+    
+}
