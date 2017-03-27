b8ac2365b3a31552a669a32702fd2811a0cd21b7
lestuckair <k.mangre@student.fontys.nl>
2017-03-27 15:33:05 +0200

    Controller FIX
    De controller werkt

--- src/Pts4/Controllers/HomeController.java
+++ src/Pts4/Controllers/HomeController.java
@@ -1,9 +1,35 @@
 package Pts4.Controllers;
+import Pts4.Classes.staticPerson;
 
-/**
- * Created by Gebruiker on 20-3-2017.
- */
-public class HomeController {
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
+public class HomeController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userName = request.getParameter("tbUserName");
+        //staticPerson sp = new staticPerson();
+        try
+        {
+            //if(sp.GetPersonData(userName))
+        //{
+           // RequestDispatcher view = request.getRequestDispatcher("urenReg.jsp");
+           // view.forward(request, response);
+        //}
+            if (userName.equals("Jan"))
+            {
+                RequestDispatcher view = request.getRequestDispatcher("urenReg.jsp");
+                view.forward(request, response);
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("Er is een fout op getreden. Error:" + e.getMessage());
+        }
+    }
 }
